36f1c1b8b0bfc6953bb2a3e6fea91365a9c45134
RaghavRaman <rghv.raman@gmail.com>
2018-01-28 14:29:11 -0500

    db connection changes

--- db.js
+++ db.js
@@ -9,15 +9,20 @@ function connectDatabase() {
         db.connect(function(err){
             if(!err) {
                 console.log('Database is connected!');
+                setTimeout(connectDatabase, 2000);
             } else {
                 console.log('Error connecting database!');
             }
+        });
+
+         db.on('error', function(err) {
+            console.log('db error', err);
             if(err.code === 'PROTOCOL_CONNECTION_LOST') { // Connection to the MySQL server is usually
               connectDatabase();                         // lost due to either server restart, or a
             } else {                                      // connnection idle timeout (the wait_timeout
               throw err;                                  // server variable configures this)
             }
-        });
+          });
 
         var del = db._protocol._delegateError;
         db._protocol._delegateError = function(err, sequence){
