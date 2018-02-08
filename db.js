3631686df99e89bfce51b436cc3035161a073a47
RaghavRaman <rghv.raman@gmail.com>
2018-02-08 00:14:21 -0500

    Pool connection added

--- db.js
+++ db.js
@@ -1,39 +1,75 @@
-var mysql = require('mysql');
+var mysql_npm = require('mysql');
 var settings = require('./settings.json');
 var db;
 
-function connectDatabase() {
-    if (!db) {
-        db = mysql.createConnection(settings["db"]);
+//- Create the connection variable
+var connection = mysql_npm.createConnection(settings["db"]);
 
-        db.connect(function(err){
-            if(!err) {
-                console.log('Database is connected!');
-                setTimeout(connectDatabase, 2000);
-            } else {
-                console.log('Error connecting database!');
-            }
-        });
+//- Establish a new connection
+connection.connect(function(err){
+    if(err) {
+        // mysqlErrorHandling(connection, err);
+        console.log("\n\t *** Cannot establish a connection with the database. ***");
 
-         db.on('error', function(err) {
-            console.log('db error', err);
-            if(err.code === 'PROTOCOL_CONNECTION_LOST') { // Connection to the MySQL server is usually
-              connectDatabase();                         // lost due to either server restart, or a
-            } else {                                      // connnection idle timeout (the wait_timeout
-              throw err;                                  // server variable configures this)
-            }
-          });
-
-        var del = db._protocol._delegateError;
-        db._protocol._delegateError = function(err, sequence){
-          if (err.fatal) {
-            console.trace('fatal error: ' + err.message);
-          }
-          return del.call(this, err, sequence);
-        };
-
+        connection = reconnect(connection);
+    }else {
+        console.log("\n\t *** New connection established with the database. ***")
     }
-    return db;
+});
+
+//- Reconnection function
+function reconnect(connection){
+    console.log("\n New connection tentative...");
+
+    //- Destroy the current connection variable
+    if(connection) connection.destroy();
+
+    //- Create a new one
+    var connection = mysql_npm.createConnection(db_config);
+
+    //- Try to reconnect
+    connection.connect(function(err){
+        if(err) {
+            //- Try to connect every 2 seconds.
+            setTimeout(reconnect, 2000);
+        }else {
+            console.log("\n\t *** New connection established with the database. ***")
+            return connection;
+        }
+    });
 }
 
-module.exports = connectDatabase();
+//- Error listener
+connection.on('error', function(err) {
+
+    //- The server close the connection.
+    if(err.code === "PROTOCOL_CONNECTION_LOST"){    
+        console.log("/!\\ Cannot establish a connection with the database. /!\\ ("+err.code+")");
+        connection = reconnect(connection);
+    }
+
+    //- Connection in closing
+    else if(err.code === "PROTOCOL_ENQUEUE_AFTER_QUIT"){
+        console.log("/!\\ Cannot establish a connection with the database. /!\\ ("+err.code+")");
+        connection = reconnect(connection);
+    }
+
+    //- Fatal error : connection variable must be recreated
+    else if(err.code === "PROTOCOL_ENQUEUE_AFTER_FATAL_ERROR"){
+        console.log("/!\\ Cannot establish a connection with the database. /!\\ ("+err.code+")");
+        connection = reconnect(connection);
+    }
+
+    //- Error because a connection is already being established
+    else if(err.code === "PROTOCOL_ENQUEUE_HANDSHAKE_TWICE"){
+        console.log("/!\\ Cannot establish a connection with the database. /!\\ ("+err.code+")");
+    }
+
+    //- Anything else
+    else{
+        console.log("/!\\ Cannot establish a connection with the database. /!\\ ("+err.code+")");
+        connection = reconnect(connection);
+    }
+
+});
+module.exports = connection;
